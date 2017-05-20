708697a9c07534784e41c0ddc82f31d00cea252e
Mantas <mgridinas@gmail.com>
2017-05-20 13:45:09 +0300

    mismatching arguments

--- core/src/main/kotlin/lt/dragas/birc/v3/core/adapter/Adapter.kt
+++ core/src/main/kotlin/lt/dragas/birc/v3/core/adapter/Adapter.kt
@@ -4,6 +4,6 @@ package lt.dragas.birc.v3.core.adapter
 /**
  * Abstractly implements interfaces for [Serializer] and [Deserializer].
  */
-abstract class Adapter<T, R> : Serializer<T>, Deserializer<R>
+abstract class Adapter<T, R> : Serializer<R>, Deserializer<T>
 {
 }
