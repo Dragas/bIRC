9292b7bb847d41ea9b22eaadfaae0ec29bf1ed85
Mantas <mgridinas@gmail.com>
2017-05-27 15:09:43 +0300

    Controllers are now interfaces.
    This supports lambda programming style

--- core/src/main/kotlin/lt/dragas/birc/v3/core/route/Controller.kt
+++ core/src/main/kotlin/lt/dragas/birc/v3/core/route/Controller.kt
@@ -1,7 +1,7 @@
 package lt.dragas.birc.v3.core.route
 
 
-abstract class Controller<T, R>
+interface Controller<T, R>
 {
-    abstract fun onTrigger(request: T): R?
+    fun onTrigger(request: T): R?
 }
