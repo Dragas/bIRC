89ed1fcde2e7cd27cff30f2e5d38706b1580749a
Mantas <mgridinas@gmail.com>
2017-06-03 15:56:49 +0300

    Refactoring

--- IRC/src/main/kotlin/lt/dragas/birc/v3/irc/message/Request.kt
+++ IRC/src/main/kotlin/lt/dragas/birc/v3/irc/message/Request.kt
@@ -1,32 +1,11 @@
 package lt.dragas.birc.v3.irc.message
 
 
-class Request(override var rawMessage: String) : Message()
+class Request(var rawMessage: String)
 {
-    var origin: String = ""
-    var type: Int = NONE
-    /**
-     * Default mode. Means that route does not work solely with private messages or channel messages.
-     */
-    companion object
-    {
-        @JvmField
-        val NONE: Int = 0
-        /**
-         * Marks route as ping route. Shouldn't be used outside [Pong]
-         */
-        @JvmField
-        val PING: Int = 4
-        /**
-         * Marks route as private message route.
-         */
-        @JvmField
-        val PRIVATE: Int = 1
-        /**
-         * Marks route as channel route.
-         */
-        @JvmField
-        val CHANNEL: Int = 2
-
-    }
+    var user = ""
+    var host = ""
+    var nickname = ""
+    var command = ""
+    var arguments: List<String> = ArrayList()
 }
--- IRC/src/main/kotlin/lt/dragas/birc/v3/irc/route/IrcRouteGroup.kt
+++ IRC/src/main/kotlin/lt/dragas/birc/v3/irc/route/IrcRouteGroup.kt
@@ -5,16 +5,19 @@ import lt.dragas.birc.v3.irc.message.Request
 import lt.dragas.birc.v3.irc.message.Response
 
 
+/**
+ * Triggering by type.
+ */
 open class IrcRouteGroup(prefix: String, vararg routes: IrcRoute) : RouteGroup<Request, Response>(prefix, *routes)
 {
-    open val type = Request.NONE
+    open val type = ""
     override fun canTrigger(request: Request): Boolean
     {
-        val canTrigger = isEnabled && request.type.and(type) == type && regex.matches(request.message)
-        if (canTrigger)
+        val canTrigger = isEnabled && request.command.equals(type, true)
+        /*if (canTrigger)
         {
             request.message = request.message.replaceFirst(regex, "")
-        }
+        }*/
         return canTrigger
     }
 }
