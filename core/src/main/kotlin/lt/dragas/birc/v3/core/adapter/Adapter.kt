21852c39e73adbfc1ab4c4899bdaf62fa235281a
Mantas <mgridinas@gmail.com>
2017-06-12 17:13:16 +0300

    changed names

--- core/src/main/kotlin/lt/dragas/birc/v3/core/adapter/Adapter.kt
+++ core/src/main/kotlin/lt/dragas/birc/v3/core/adapter/Adapter.kt
@@ -4,6 +4,7 @@ package lt.dragas.birc.v3.core.adapter
 /**
  * Abstractly implements interfaces for [Serializer] and [Deserializer].
  */
-abstract class Adapter<T, R> : Serializer<R>, Deserializer<T>
+abstract class Adapter<Request, Response> : Deserializer<Request>, Serializer<Response>
 {
+
 }
