7d7be2f9c3f14b60abbddb12696e30e4aa1ea65a
Mantas <mgridinas@gmail.com>
2017-06-28 09:07:33 +0300

    docs

--- IRC/src/main/kotlin/lt/dragas/birc/v3/irc/route/CommandRoute.kt
+++ IRC/src/main/kotlin/lt/dragas/birc/v3/irc/route/CommandRoute.kt
@@ -5,7 +5,13 @@ import lt.dragas.birc.v3.irc.message.Response
 import java.util.regex.Pattern
 
 /**
- * A command route
+ * A command route.
+ *
+ * Handles your usual commands like 422, 114. The difference here is that pattern argument is omitted,
+ * since nearly always you won't need to act differently if there's no particular argument in response.
+ *
+ * @param type command this route responds to, usually defined in [IrcRouter]
+ * @param callback a callback to invoke when this route triggers.
  */
 class CommandRoute(type: String, callback: (Request) -> Response) : IrcRoute(type, Pattern.compile(""), callback)
 {
