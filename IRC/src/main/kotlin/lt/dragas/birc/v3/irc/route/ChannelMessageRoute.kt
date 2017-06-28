87abe4e60a2170062e9c8fc9a680da6fbfc85efa
Mantas <mgridinas@gmail.com>
2017-06-28 09:11:31 +0300

    Docs

--- IRC/src/main/kotlin/lt/dragas/birc/v3/irc/route/ChannelMessageRoute.kt
+++ IRC/src/main/kotlin/lt/dragas/birc/v3/irc/route/ChannelMessageRoute.kt
@@ -8,6 +8,8 @@ import lt.dragas.birc.v3.irc.message.Response
  *
  * All it does is check whether or not first argument starts with a #, which means that the message
  * is meant for the channel.
+ * @param pattern a pattern which the private message is tested against
+ * @param callback callback to invoke when testing against pattern succeeds
  */
 class ChannelMessageRoute(pattern: String, callback: (Request) -> Response) : MessageRoute(pattern, callback)
 {
--- IRC/src/main/kotlin/lt/dragas/birc/v3/irc/route/PingRoute.kt
+++ IRC/src/main/kotlin/lt/dragas/birc/v3/irc/route/PingRoute.kt
@@ -6,7 +6,9 @@ import lt.dragas.birc.v3.irc.route.IrcRouter.Companion.PING
 import java.util.regex.Pattern
 
 /**
- * Handles pings from IRC servers
+ * Handles pings from IRC servers.
+ *
+ * Equivalent to calling `object : IrcRote(IrcRouter.PING, Pattern.Compile(""), callback)`
  */
 @Deprecated("Use command route instead", ReplaceWith("CommandRoute(PING, callback)"))
 class PingRoute(callback: (Request) -> Response) : IrcRoute(PING, Pattern.compile(""), callback)
