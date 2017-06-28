efc335350220c18f6613ab9bc52f295c26383839
Mantas <mgridinas@gmail.com>
2017-06-28 09:12:27 +0300

    docs

--- IRC/src/main/kotlin/lt/dragas/birc/v3/irc/route/IrcRoute.kt
+++ IRC/src/main/kotlin/lt/dragas/birc/v3/irc/route/IrcRoute.kt
@@ -9,7 +9,8 @@ import java.util.regex.Pattern
  * An IRC implementation of [Route] objects.
  *
  * By default, they only test if provided type matches the request type,
- * implementations should test the request arguments according to its type
+ * implementations should test the request arguments according to its type, for example
+ * private message routes should test whether or not private message requests are meant for channels.
  */
 abstract class IrcRoute(open protected val type: String, pattern: Pattern, callback: (Request) -> Response) : Route<Request, Response>(pattern, callback)
 {
