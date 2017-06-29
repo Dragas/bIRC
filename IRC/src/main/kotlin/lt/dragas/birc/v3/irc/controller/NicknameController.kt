a7b1e7d180b7a490e2a803c7c41bfe2b7a243e23
Mantas <mgridinas@gmail.com>
2017-06-29 16:09:00 +0300

    Nickname Controller

--- IRC/src/main/kotlin/lt/dragas/birc/v3/irc/controller/NicknameController.kt
+++ IRC/src/main/kotlin/lt/dragas/birc/v3/irc/controller/NicknameController.kt
@@ -1,30 +1,74 @@
 package lt.dragas.birc.v3.irc.controller
 
+import lt.dragas.birc.v3.irc.Settings
 import lt.dragas.birc.v3.irc.message.Request
 import lt.dragas.birc.v3.irc.message.Response
+import lt.dragas.birc.v3.irc.route.Command
 import lt.dragas.birc.v3.irc.route.IrcRouter
 import java.util.concurrent.atomic.AtomicInteger
 
 /**
  * Handles nickname changes when necessary.
  */
-class NicknameController(vararg val nicknames: String)
+class NicknameController(private val settings: Settings)
 {
-    private var lastUsedNickname = AtomicInteger(1)
+    private var lastUsedNickname = AtomicInteger(0)
+    private var currentNickname = ""
 
     fun onNicknameChangeRequest(request: Request): Response
     {
-        val nickname = nicknames.getOrNull(lastUsedNickname.getAndAdd(1)) ?: "Botter-${lastUsedNickname.getAndAdd(1)}"
-        return Response("nick", nickname)
+        val nickname = settings.nicknames.getOrElse(lastUsedNickname.getAndAdd(1), { it -> "${settings.nicknames[0]}-$it}" })
+        return Response(Command.NICK, nickname)
+    }
+
+    fun onNicknameInitialize(request: Request): Response
+    {
+        val response = onNicknameChangeRequest(request)
+        currentNickname = response.arguments[0]
+        val userResponse = Response(Command.USER, settings.user, settings.mode, "*", ":${settings.realname}")
+        response.otherResponses.add(userResponse)
+        return response
+    }
+
+    /**
+     * Assumes self nickname change.
+     */
+    fun onNicknameChange(request: Request): Response?
+    {
+        if (request.nickname == currentNickname)
+            currentNickname = request.arguments[0]
+        return null
     }
 
     companion object
     {
-        lateinit var instance: NicknameController
+
         @JvmStatic
-        fun initialize(router: IrcRouter, nicknames: Array<out String>)
+        val instance: NicknameController by lazy()
         {
-            instance = NicknameController(*nicknames)
+            val controller = NicknameController(setttings)
+            controller
         }
+
+        @JvmStatic
+        private var setttings: Settings = Settings()
+
+        @JvmStatic
+        fun initialize(router: IrcRouter, settings: Settings)
+        {
+            setttings = settings
+            router.`when`(Command.ERR_NICKCOLLISION, instance::onNicknameChangeRequest)
+            router.`when`(Command.ERR_NICKNAMEINUSE, instance::onNicknameChangeRequest)
+            router.`when`(Command.ERR_NOTREGISTERED, instance::onNicknameInitialize)
+            router.`when`(Command.AUTH, instance::onNicknameInitialize)
+            router.`when`(Command.NICK, instance::onNicknameChange)
+        }
+
+        @JvmStatic
+        val currentNickname: String
+            get()
+            {
+                return instance.currentNickname
+            }
     }
 }
