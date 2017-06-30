22bfff524941149c4ad2446da45780bff29b7d57
Mantas <mgridinas@gmail.com>
2017-06-30 10:38:21 +0300

    semantics

--- irc/src/main/kotlin/lt/saltyjuice/dragas/chatty/v3/irc/Settings.kt
+++ irc/src/main/kotlin/lt/saltyjuice/dragas/chatty/v3/irc/Settings.kt
@@ -8,16 +8,16 @@ import java.net.SocketAddress
  */
 open class Settings
 {
-    var address: String = ""
-    var port: Int = 6667
-    var user: String = "default"
-    var mode: String = "default"
-    var unused: String = "default"
-    var realname: String = ":default"
-    var nicknames: ArrayList<String> = ArrayList()
-    var channels: ArrayList<String> = ArrayList()
+    open var address: String = ""
+    open var port: Int = 6667
+    open var user: String = "default"
+    open var mode: String = "default"
+    open var unused: String = "default"
+    open var realname: String = ":default"
+    open var nicknames: ArrayList<String> = ArrayList()
+    open var channels: ArrayList<String> = ArrayList()
 
-    fun getSocketAddress(): SocketAddress
+    open fun getSocketAddress(): SocketAddress
     {
         return InetSocketAddress(address, port)
     }
