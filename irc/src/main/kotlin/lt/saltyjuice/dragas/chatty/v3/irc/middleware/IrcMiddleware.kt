a418c0ea3f150bc9c6f108ac986681134c4fc7c0
Mantas <mgridinas@gmail.com>
2017-06-30 15:49:19 +0300

    added middleware wrapper

---
+++ irc/src/main/kotlin/lt/saltyjuice/dragas/chatty/v3/irc/middleware/IrcMiddleware.kt
@@ -0,0 +1,13 @@
+package lt.saltyjuice.dragas.chatty.v3.irc.middleware
+
+import lt.saltyjuice.dragas.chatty.v3.core.route.Middleware
+import lt.saltyjuice.dragas.chatty.v3.irc.message.Request
+
+
+/**
+ * Wrapper for Core middleware
+ */
+abstract class IrcMiddleware : Middleware<Request>()
+{
+
+}
