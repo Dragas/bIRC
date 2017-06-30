b2186f306c0ddff38ec85d652bf3af80f4469f20
Mantas <mgridinas@gmail.com>
2017-06-30 11:13:43 +0300

    a fallback

--- irc/src/main/kotlin/lt/saltyjuice/dragas/chatty/v3/irc/route/IrcRoute.kt
+++ irc/src/main/kotlin/lt/saltyjuice/dragas/chatty/v3/irc/route/IrcRoute.kt
@@ -12,10 +12,12 @@ import java.util.regex.Pattern
  * implementations should test the request arguments according to its type, for example
  * private message routes should test whether or not private message requests are meant for channels.
  */
-abstract class IrcRoute(open protected val type: Command, pattern: Pattern, callback: (Request) -> Response?) : Route<Request, Response>(pattern, callback)
+abstract class IrcRoute(open protected val type: String, pattern: Pattern, callback: (Request) -> Response?) : Route<Request, Response>(pattern, callback)
 {
+    constructor(command: Command, pattern: Pattern, callback: (Request) -> Response?) : this(command.value, pattern, callback)
+
     override fun canTrigger(request: Request): Boolean
     {
-        return request.command == type.value
+        return request.command == type
     }
 }
