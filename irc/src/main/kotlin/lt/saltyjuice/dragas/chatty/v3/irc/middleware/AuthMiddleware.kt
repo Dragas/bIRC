08814b22a024c0d80118d1be5bb5a248df4e5af0
Mantas <mgridinas@gmail.com>
2017-06-30 16:00:57 +0300

    Added auth middleware

---
+++ irc/src/main/kotlin/lt/saltyjuice/dragas/chatty/v3/irc/middleware/AuthMiddleware.kt
@@ -0,0 +1,14 @@
+package lt.saltyjuice.dragas.chatty.v3.irc.middleware
+
+import lt.saltyjuice.dragas.chatty.v3.irc.message.Request
+
+
+class AuthMiddleware : IrcMiddleware()
+{
+    override val name: String = "AUTH"
+
+    override fun handle(request: Request): Boolean
+    {
+        return request.arguments[0] == "AUTH"
+    }
+}
