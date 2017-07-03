a2ae03cbcb366acf53c42b7e49e95f29476ae55b
Mantas <mgridinas@gmail.com>
2017-07-03 17:31:42 +0300

    allows inheritence

--- irc/src/main/kotlin/lt/saltyjuice/dragas/chatty/v3/irc/routing/IrcRouter.kt
+++ irc/src/main/kotlin/lt/saltyjuice/dragas/chatty/v3/irc/routing/IrcRouter.kt
@@ -5,7 +5,7 @@ import lt.saltyjuice.dragas.chatty.v3.irc.message.Request
 import lt.saltyjuice.dragas.chatty.v3.irc.message.Response
 
 
-class IrcRouter : Router<Request, Response>()
+open class IrcRouter : Router<Request, Response>()
 {
     override fun builder(): IrcRouteBuilder
     {
