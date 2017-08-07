b4d5f8fda7d64bb5b8de081b8b962ef4a9a92a0a
Mantas Gridinas <mgridinas@gmail.com>
2017-08-07 15:30:00 +0300

    fixes an issue where resource is not considered to be a stream

--- birc/src/main/kotlin/lt/saltyjuice/dragas/chatty/v3/birc/Main.kt
+++ birc/src/main/kotlin/lt/saltyjuice/dragas/chatty/v3/birc/Main.kt
@@ -4,14 +4,15 @@ package lt.saltyjuice.dragas.chatty.v3.birc
 
 import com.google.gson.Gson
 import kotlinx.coroutines.experimental.runBlocking
-import java.io.FileReader
 import java.io.IOException
+import java.io.InputStreamReader
+import java.io.Reader
 
 
 fun main(args: Array<String>) = runBlocking<Unit>
 {
     val gson = Gson()
-    val settings = gson.fromJson<BIrcSettings>(getReader("../lib/settings.json"), BIrcSettings::class.java)
+    val settings = gson.fromJson<BIrcSettings>(getReader("settings.json"), BIrcSettings::class.java)
     val client = BIrcClient(settings)
     client.initialize()
     client.connect()
@@ -35,9 +36,11 @@ fun main(args: Array<String>) = runBlocking<Unit>
     }
 }
 
-fun getReader(filename: String): FileReader
+fun getReader(filename: String): Reader
 {
-    //val classLoader = Thread.currentThread().contextClassLoader
-    val fileReader = FileReader(filename)
+    val classLoader = Thread.currentThread().contextClassLoader
+    val stream = classLoader.getResourceAsStream(filename)
+    val fileReader = InputStreamReader(stream)
+    //val fileReader = FileReader(filename)
     return fileReader
 }
