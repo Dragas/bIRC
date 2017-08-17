26dd782ae10df09f0a3e88c0a2c3901a983c3622
Mantas Gridinas <mgridinas@gmail.com>
2017-08-17 14:54:43 +0300

    Working sample that keeps the connection alive

--- discord/src/main/kotlin/lt/saltyjuice/dragas/chatty/v3/discord/Main.kt
+++ discord/src/main/kotlin/lt/saltyjuice/dragas/chatty/v3/discord/Main.kt
@@ -3,9 +3,8 @@
 package lt.saltyjuice.dragas.chatty.v3.discord
 
 import kotlinx.coroutines.experimental.runBlocking
+import lt.saltyjuice.dragas.chatty.v3.discord.main.DiscordClient
 import org.glassfish.tyrus.core.TyrusEndpointWrapper
-import java.io.BufferedReader
-import java.io.InputStreamReader
 import java.util.logging.Level
 import java.util.logging.Logger
 
@@ -17,6 +16,11 @@ fun main(args: Array<String>) = runBlocking<Unit>
     val discordClient = DiscordClient(gatewayResponse)
     discordClient.initialize()
     discordClient.connect()
-    println("Press any key to stop")
-    BufferedReader(InputStreamReader(System.`in`)).readLine()
+    discordClient.onConnect()
+    while (discordClient.isConnected())
+    {
+        discordClient.run()
+    }
+    discordClient.disconnect()
+    discordClient.onDisconnect()
 }
