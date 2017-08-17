0503a306415e7b7d7354787d6a94e12c65b536ef
Mantas Gridinas <mgridinas@gmail.com>
2017-08-17 14:59:51 +0300

    reflection based type checking

--- discord/src/main/kotlin/lt/saltyjuice/dragas/chatty/v3/discord/route/DiscordRoute.kt
+++ discord/src/main/kotlin/lt/saltyjuice/dragas/chatty/v3/discord/route/DiscordRoute.kt
@@ -4,7 +4,7 @@ import lt.saltyjuice.dragas.chatty.v3.discord.message.request.OPRequest
 import lt.saltyjuice.dragas.chatty.v3.discord.message.response.OPResponse
 import lt.saltyjuice.dragas.chatty.v3.websocket.route.WebSocketRoute
 
-open class DiscordRoute : WebSocketRoute<OPRequest<*>, OPResponse<*>>()
+open class DiscordRoute<Request : OPRequest<*>, Response : OPResponse<*>> : WebSocketRoute<Request, Response>()
 {
 
 }
--- discord/src/main/kotlin/lt/saltyjuice/dragas/chatty/v3/discord/route/DiscordRouter.kt
+++ discord/src/main/kotlin/lt/saltyjuice/dragas/chatty/v3/discord/route/DiscordRouter.kt
@@ -2,11 +2,28 @@ package lt.saltyjuice.dragas.chatty.v3.discord.route
 
 import lt.saltyjuice.dragas.chatty.v3.discord.message.request.OPRequest
 import lt.saltyjuice.dragas.chatty.v3.discord.message.response.OPResponse
+import lt.saltyjuice.dragas.chatty.v3.websocket.route.WebSocketRouteBuilder
 import lt.saltyjuice.dragas.chatty.v3.websocket.route.WebSocketRouter
 
 open class DiscordRouter : WebSocketRouter<OPRequest<*>, OPResponse<*>>()
 {
-    override fun builder(): DiscordRouteBuilder
+    open fun add(route: DiscordRouteBuilder<*, *>)
+    {
+        add(route as WebSocketRouteBuilder<OPRequest<*>, OPResponse<*>>)
+    }
+
+    /**
+     * Discord API implementations should use [discordBuilder] instead, as it permits specifying what
+     */
+    override fun builder(): WebSocketRouteBuilder<OPRequest<*>, OPResponse<*>>
+    {
+        return discordBuilder()
+    }
+
+    /**
+     * Returns a discord route builder, which can specify what
+     */
+    open fun <Request : OPRequest<*>, Response : OPResponse<*>> discordBuilder(): DiscordRouteBuilder<Request, Response>
     {
         return DiscordRouteBuilder()
     }
