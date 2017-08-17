6d4a9bca7319f8a37344a4a68f5a5dfb8dcc3abc
Mantas Gridinas <mgridinas@gmail.com>
2017-08-17 15:39:28 +0300

    Added HTTP implementations

---
+++ discord/src/main/kotlin/lt/saltyjuice/dragas/chatty/v3/discord/api/DiscordAPI.kt
@@ -0,0 +1,11 @@
+package lt.saltyjuice.dragas.chatty.v3.discord.api
+
+import lt.saltyjuice.dragas.chatty.v3.discord.message.request.GatewayInit
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface DiscordAPI
+{
+    @GET("gateway/bot/")
+    fun gatewayInit(): Call<GatewayInit>
+}
