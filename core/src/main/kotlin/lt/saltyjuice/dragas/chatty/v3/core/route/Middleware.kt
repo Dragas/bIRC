9ccade240c97eabe47fa62d6816326917db63ed2
Mantas Gridinas <mgridinas@gmail.com>
2017-08-19 17:20:31 +0300

    fixes issue #12

--- core/src/main/kotlin/lt/saltyjuice/dragas/chatty/v3/core/route/Middleware.kt
+++ core/src/main/kotlin/lt/saltyjuice/dragas/chatty/v3/core/route/Middleware.kt
@@ -11,21 +11,6 @@ import lt.saltyjuice.dragas.chatty.v3.core.middleware.BeforeMiddleware
  */
 abstract class Middleware<Request, Response>() : BeforeMiddleware<Request>, AfterMiddleware<Response>
 {
-    /**
-     * Used for caching purposes. Also makes it so that you can get this middleware back by calling
-     * [Middleware.getMiddleware] with that name.
-     */
-    abstract val name: String
-
-    /**
-     * Tests whether or not particular request passes the middleware.
-     */
-    @Deprecated("Use Before and After methods, instead. this calls before")
-    open fun handle(request: Request): Boolean
-    {
-        return before(request)
-    }
-
     override fun before(request: Request): Boolean
     {
         return true
@@ -34,34 +19,5 @@ abstract class Middleware<Request, Response>() : BeforeMiddleware<Request>, Afte
     override fun after(response: Response): Boolean
     {
         return true
-    }
-
-    init
-    {
-        registerMiddleware(this)
-    }
-
-    companion object
-    {
-        @JvmStatic
-        private val middlewareCache = ArrayList<Middleware<*, *>>()
-
-        @JvmStatic
-        @Throws(Exception::class)
-        fun getMiddleware(name: String): Middleware<*, *>
-        {
-            return middlewareCache.firstOrNull { name == it.name } ?: throw Exception("No such middleware")
-        }
-
-        @JvmStatic
-        @Throws(Exception::class)
-        fun registerMiddleware(middleware: Middleware<*, *>)
-        {
-            if (middlewareCache.firstOrNull { it.name == middleware.name } != null)
-                throw Exception("Middleware under name ${middleware.name} is already registered")
-            if (middlewareCache.contains(middleware))
-                throw Exception("This particular middleware is already registered")
-            middlewareCache.add(middleware)
-        }
     }
 }
