23378001054d61dd56c60f1ebfb4fb86085eea84
Mantas Gridinas <mgridinas@gmail.com>
2017-08-24 13:58:50 +0300

    Added a test for retrofit

---
+++ discord/src/test/kotlin/lt/saltyjuice/dragas/chatty/v3/discord/RetrofitTest.kt
@@ -0,0 +1,16 @@
+package lt.saltyjuice.dragas.chatty.v3.discord
+
+import lt.saltyjuice.dragas.chatty.v3.discord.api.Utility
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class RetrofitTest
+{
+    @Test
+    fun retrofitCompilesCorrectly()
+    {
+        Utility.discordAPI
+    }
+}
