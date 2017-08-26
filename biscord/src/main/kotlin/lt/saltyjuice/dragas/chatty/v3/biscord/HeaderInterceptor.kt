6856faaac51e6a9610041e7d06bf54bf33a892d6
Mantas Gridinas <mgridinas@gmail.com>
2017-08-26 09:33:02 +0300

    adds header interceptor

---
+++ biscord/src/main/kotlin/lt/saltyjuice/dragas/chatty/v3/biscord/HeaderInterceptor.kt
@@ -0,0 +1,20 @@
+package lt.saltyjuice.dragas.chatty.v3.biscord
+
+import okhttp3.Interceptor
+import okhttp3.Response
+
+/**
+ * Adds provided headers to all requests emitted by OKHttp client with this interceptor
+ */
+open class HeaderInterceptor(vararg private val headers: Pair<String, String>) : Interceptor
+{
+    override fun intercept(chain: Interceptor.Chain): Response
+    {
+        val request = chain.request().newBuilder()
+        headers.forEach()
+        { (key, value) ->
+            request.addHeader(key, value)
+        }
+        return chain.proceed(request.build())
+    }
+}
--- biscord/src/main/kotlin/lt/saltyjuice/dragas/chatty/v3/biscord/Main.kt
+++ biscord/src/main/kotlin/lt/saltyjuice/dragas/chatty/v3/biscord/Main.kt
@@ -5,6 +5,7 @@ import lt.saltyjuice.dragas.chatty.v3.discord.api.Utility
 
 fun main(args: Array<String>) = runBlocking<Unit>
 {
+    Utility.okHttpBuilder.addInterceptor(HeaderInterceptor(Pair("X-Requested-With", "XMLHttpRequest")))
     val gatewayResponse = Utility.discordAPI.gatewayInit().execute().body()!!
     val discordClient = BiscordClient(gatewayResponse)
     discordClient.initialize()
