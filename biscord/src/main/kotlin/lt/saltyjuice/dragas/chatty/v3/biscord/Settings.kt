e4ceb4c230a892a698715534b170fd96ba17a169
Dragas <mgridinas@gmail.com>
2017-08-27 14:50:32 +0300

    changed how API keys are stored.

--- biscord/src/main/kotlin/lt/saltyjuice/dragas/chatty/v3/biscord/Settings.kt
+++ biscord/src/main/kotlin/lt/saltyjuice/dragas/chatty/v3/biscord/Settings.kt
@@ -3,8 +3,10 @@ package lt.saltyjuice.dragas.chatty.v3.biscord
 object Settings
 {
     @JvmStatic
-    val MASHAPE_KEY = System.getenv("MASHAPE_KEY") ?: throw NullPointerException("MASHAPE_KEY environmental variable must be present")
+    val MASHAPE_HEADER = "X-Mashape-Key"
+
+    val MASHAPE_KEY_ENV = "MASHAPE_KEY"
 
     @JvmStatic
-    val MASHAPE_HEADER = "X-Mashape-Key"
+    val MASHAPE_KEY = System.getenv(MASHAPE_KEY_ENV) ?: throw NullPointerException("$MASHAPE_KEY_ENV environmental variable must be present")
 }
