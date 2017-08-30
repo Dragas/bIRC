f51abcb5acb839ff7de2665b34e1a7ce967dc4f7
Mantas Gridinas <mgridinas@gmail.com>
2017-08-30 13:31:47 +0300

    proper argument limiting

--- core/src/main/kotlin/lt/saltyjuice/dragas/chatty/v3/core/route/After.kt
+++ core/src/main/kotlin/lt/saltyjuice/dragas/chatty/v3/core/route/After.kt
@@ -1,12 +1,15 @@
 package lt.saltyjuice.dragas.chatty.v3.core.route
 
+import lt.saltyjuice.dragas.chatty.v3.core.middleware.AfterMiddleware
 import kotlin.reflect.KClass
 
 
 /**
  * Marks that particular callback or controller or client uses mentioned middleware before using that generated response.
+ *
+ * Behaves like [Before].
  */
 @MustBeDocumented
 @Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION)
 @Retention()
-annotation class After(vararg val value: KClass<*>)
+annotation class After(vararg val value: KClass<out AfterMiddleware<*>>)
