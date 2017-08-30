1b7f0fd7f68d13fc21c89d1e07001bd87591bf34
Mantas Gridinas <mgridinas@gmail.com>
2017-08-30 16:42:40 +0300

    refactored controllers

--- core/src/test/kotlin/lt/saltyjuice/dragas/chatty/v3/core/mock/BadMockController.kt
+++ core/src/test/kotlin/lt/saltyjuice/dragas/chatty/v3/core/mock/BadMockController.kt
@@ -4,14 +4,14 @@ import lt.saltyjuice.dragas.chatty.v3.core.route.*
 
 @Before(MockBeforeMiddleware::class)
 @After(MockAfterMiddleware::class)
-class BadMockController() : Controller<MockRequest, MockResponse>()
+class BadMockController() : Controller<MockResponse>()
 {
     @On(MockRequest::class)
     @When("someMethod")
     @Before(MockBeforeMiddleware::class) // dont do this
     @After(MockAfterMiddleware::class) // will throw duplicate middleware exception
-    fun badCallback(request: MockRequest): MockResponse
+    fun badCallback(request: MockRequest)
     {
-        return MockResponse("", "")
+        writeResponse(MockResponse("", ""))
     }
 }
